7106c64a5ad525e2fa1efd7e320ecda9a87afefd
Mario Ribeiro Garcia Junior <mario.rg.junior7@gmail.com>
2021-10-08 09:18:43 -0300

    Add moviment for the enemy

---
+++ Space Shooter/Assets/Script/Enemy_Script.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Script : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, -speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
