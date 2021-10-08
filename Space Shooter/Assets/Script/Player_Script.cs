3ae93f4151fb405132b4b8b3947ddb8083e3d349
Mario Ribeiro Garcia Junior <mario.rg.junior7@gmail.com>
2021-10-08 09:09:52 -0300

    Add movement limits

--- Space Shooter/Assets/Script/Player_Script.cs
+++ Space Shooter/Assets/Script/Player_Script.cs
@@ -6,6 +6,8 @@ public class Player_Script : MonoBehaviour
 {
 
     private Rigidbody2D rb;
+    private float width;
+    private float height;
 
     public float speed = 10f;
 
@@ -14,6 +16,8 @@ public class Player_Script : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        height = Camera.main.orthographicSize;
+        width = height * Camera.main.aspect;
     }
 
     // Update is called once per frame
@@ -23,5 +27,29 @@ public class Player_Script : MonoBehaviour
         float verticalMove = Input.GetAxis("Vertical");
 
         rb.velocity = new Vector2(horizontalMove, verticalMove) * speed;
+
+        borderlands();
     }
+
+    private void borderlands()
+    {
+        if (transform.position.x < -width)
+        {
+            transform.position = new Vector2(width, transform.position.y);
+        }
+        else if (transform.position.x > width)
+        {
+            transform.position = new Vector2(-width, transform.position.y);
+        }
+
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector2(transform.position.x, 0);
+        }
+        else if (transform.position.y < -4)
+        {
+            transform.position = new Vector2(transform.position.x, -4);
+        }
+    }
+
 }
