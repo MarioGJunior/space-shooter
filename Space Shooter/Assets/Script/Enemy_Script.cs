48a27565edc9e6ed996f20b01004a202b66f0630
Mario Ribeiro Garcia Junior <mario.rg.junior7@gmail.com>
2021-10-11 10:26:46 -0300

    Added sound when enemy die

--- Space Shooter/Assets/Script/Enemy_Script.cs
+++ Space Shooter/Assets/Script/Enemy_Script.cs
@@ -6,6 +6,7 @@ public class Enemy_Script : MonoBehaviour
 {
 
     private Rigidbody2D rb;
+    private AudioSource sound;
 
     [SerializeField]
     private float speed = 5f;
@@ -15,6 +16,7 @@ public class Enemy_Script : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(0, -speed);
+        sound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -30,10 +32,20 @@ public class Enemy_Script : MonoBehaviour
     {
         if(collider.tag == "Laser")
         {
+            sound.Play();
             Score_Script.ScoreUpdate(5);
         }
 
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<BoxCollider2D>().enabled = false;
+        StartCoroutine(Explosion());
         Destroy(collider.gameObject);
+        
+    }
+
+    public IEnumerator Explosion()
+    {
+        yield return new WaitForSeconds(1);
         Destroy(gameObject);
     }
 
